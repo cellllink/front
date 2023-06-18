92d04d2ccb03173f91864853dd608a94aee0eb8e
care <710783534@qq.com>
2023-06-19 07:47:41 +0800

    123

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { RecoilRoot } from "recoil";
-import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
     <RecoilRoot>
       <HashRouter>
-        <Switch>
-          {/* <Route path="/auth" component={Auth}></Route>
+        {/* <Switch>
+          <Route path="/auth" component={Auth}></Route>
             <Route path="/space" component={Space}></Route>
             <Route path="/share" component={Share}></Route>
-            <Redirect to="/space" /> */}
-        </Switch>
+            <Redirect to="/space" />
+        </Switch> */}
       </HashRouter>
     </RecoilRoot>
   );

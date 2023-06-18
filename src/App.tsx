2be9c71e8e18119d0efdfd58a9014269489fd67c
care <710783534@qq.com>
2023-06-18 12:26:27 +0800

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { RecoilRoot } from "recoil";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecoilRoot>
+      <HashRouter>
+        <Switch>
+          {/* <Route path="/auth" component={Auth}></Route>
+            <Route path="/space" component={Space}></Route>
+            <Route path="/share" component={Share}></Route>
+            <Redirect to="/space" /> */}
+        </Switch>
+      </HashRouter>
+    </RecoilRoot>
   );
 }
 

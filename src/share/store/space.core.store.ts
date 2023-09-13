8117908d6b7fc178c3a59a659eef206e0e6afbd7
care <710783534@qq.com>
2023-09-13 23:37:39 +0800

    1

---
+++ src/share/store/space.core.store.ts
@@ -0,0 +1,57 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import { Organization, User } from "../type/core.type";
+
+interface CoreState {
+  user: null | User; // 当前登陆用户信息
+  organization: null | Organization; // 当前登陆用户 选择的组织
+  userOrganizations: Organization[]; // 当前登陆用户 已加入的组织列表
+}
+interface CoreMutation {
+  reset: () => void;
+  update: (state: Partial<CoreState>) => void;
+}
+interface CoreAction {
+  updateUser: () => Promise<void>;
+  updateOrganization: () => Promise<void>;
+  updateUserOrganizations: () => Promise<void>;
+}
+interface CoreStore extends CoreState, CoreMutation, CoreAction {}
+
+const initialState: CoreState = {
+  user: null,
+  organization: null,
+  userOrganizations: [],
+};
+
+export const useCoreStore = create(
+  persist<CoreStore>(
+    (set, get) => ({
+      ...initialState,
+
+      update: (state: Partial<CoreState>) => {
+        set(state);
+      },
+
+      reset: () => {
+        set(initialState);
+      },
+
+      updateUser: () => {
+        set({ user: null });
+        return Promise.reject();
+      },
+
+      updateOrganization: () => {
+        set({ user: null });
+        return Promise.reject();
+      },
+
+      updateUserOrganizations: () => {
+        set({ user: null });
+        return Promise.reject();
+      },
+    }),
+    { name: "CoreStore", storage: createJSONStorage(() => localStorage) }
+  )
+);
